168c903da1e83cbf35806257863e80778779cd80
Thamiris Vicente <thamiavicente@gmail.com>
2025-07-28 17:37:32 +0200

    Config: Remove unused files

--- app/date.tsx
+++
@@ -1,9 +0,0 @@
-import { format } from "date-fns";
-
-export default function DateComponent({ dateString = '11-06-1994' }: { dateString: string }) {
-  return (
-    <time dateTime={dateString}>
-      {format(new Date(dateString), "LLLL	d, yyyy")}
-    </time>
-  );
-}
