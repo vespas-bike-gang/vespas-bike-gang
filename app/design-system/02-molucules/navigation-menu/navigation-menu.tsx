6bbe03ceae23c5fac0e8c143fb5d9ca0e016b1c1
Thamiris Vicente <thamiavicente@gmail.com>
2025-07-30 15:18:59 +0200

    Feature: Connect navigation menu component to contentful

--- app/design-system/02-molucules/navigation-menu/navigation-menu.tsx
+++ app/design-system/02-molucules/navigation-menu/navigation-menu.tsx
@@ -1,11 +1,33 @@
 import styles from './navigation-menu.module.scss'
 
+import { getEntriesByTitle } from '@/lib/contentful'
 import { LogoSymbol } from '@/app/design-system/01-atoms/icons'
 
-export default function NavigationMenu() {
+type NavigationMenuItemFields = {
+    fields: {
+        label: string;
+        href: string;
+    }
+    contentTypeId: string
+}
+
+export default async function NavigationMenu () {
+    const entries = await getEntriesByTitle({
+        contentType: 'navigationMenu',
+        title: 'Header Menu'
+    })
+    const data = entries.itens || []
+
     return (
         <nav className={styles['navigation']}>
-            <LogoSymbol></LogoSymbol>
+            <LogoSymbol color="#ffffff" width="54" height="40"></LogoSymbol>
+            <ol>
+                { data.map((entry: NavigationMenuItemFields, index: string | number) => (
+                    <li key={index}>
+                        <a href={entry.fields.href}>{entry.fields.label}</a>
+                    </li>
+                ))}
+            </ol>
         </nav>
     )
 }
