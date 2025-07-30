5078d6e13e65d260ceb370bae245989c60eae57d
Thamiris Vicente <thamiavicente@gmail.com>
2025-07-30 15:11:32 +0200

    Config: Fix route draft

--- app/api/disable-draft/route.ts
+++ app/api/disable-draft/route.ts
@@ -1,6 +1,7 @@
 import { draftMode } from "next/headers";
 
 export async function GET(request: Request) {
-  const { isEnabled } = await draftMode()
-  return new Response(`Draft mode is ${isEnabled}`);
+  const draft = await draftMode()
+  draft.disable()
+  return new Response(`Draft mode is disabled`);
 }
