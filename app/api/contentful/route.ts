20c3e84d7f27725236824dac2d198b5b24f14da6
Thamiris Vicente <thamiavicente@gmail.com>
2025-09-29 21:47:36 +0200

    Fix: Return all data from request on get entries

--- app/api/contentful/route.ts
+++ app/api/contentful/route.ts
@@ -5,5 +5,5 @@ export async function POST(request: Request) {
     const requestBody = await request.json()
     const entries = await contentful.getEntriesByTitle(requestBody)
     if(!entries) return NextResponse.json('Error')
-    return NextResponse.json(entries.items[0]?.fields)
+    return NextResponse.json(entries)
 }
